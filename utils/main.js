cc4ff5091e0de687fcecdafe24815e8d038f062b
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-06-01 19:56:23 +0800

    up

--- utils/main.js
+++ utils/main.js
@@ -228,10 +228,10 @@ self.Utils = Utils; // export for iOS 14- qwq
     });
   `);
   */
-  /* sdk.51.la */
+  /* sdk.51.la 
   Utils.loadJS('!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"Jj67QM9XKxdyBUKP",ck:"Jj67QM9XKxdyBUKP"});');
   Utils.loadJS('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8369333813205733');
-}());
+*/}());
 // cookie
 Utils.lazyload(() => {
   const fd = Utils.formatDate(navigator.language);
